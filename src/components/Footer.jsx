1a8852cae3563966f1882168c9cf22b9a4c914a1
mahmoud.wahba.dev <mahmoud.wahba.dev@gmail.com>
2025-08-13 04:31:21 +0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
   const navigation = [
     { name: t("nav.home"), href: "/" },
     { name: t("nav.about"), href: "/about" },
-    { name: t("nav.services"), href: "/services" },
+    { name: t("nav.services"), href: "/#services_sec" },
     { name: t("nav.contact"), href: "/contact" },
   ];
 
